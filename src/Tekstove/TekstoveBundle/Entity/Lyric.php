9199f39ff66dcf04de9959c5823eb0e5b1878af9
angelk <angel.koilov@gmail.com>
2015-10-31 22:19:51 +0200

    lyric entity fixes

--- src/Tekstove/TekstoveBundle/Entity/Lyric.php
+++ src/Tekstove/TekstoveBundle/Entity/Lyric.php
@@ -4,8 +4,9 @@ namespace Tekstove\TekstoveBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\Common\Collections\ArrayCollection;
-
 use Symfony\Component\Validator\Constraints as SymfonyValidator;
+
+use Tekstove\TekstoveBundle\Entity\Language;
 
 /**
  * Lyric
@@ -15,6 +16,7 @@ use Symfony\Component\Validator\Constraints as SymfonyValidator;
  */
 class Lyric
 {
+
     /**
      * @var integer
      *
@@ -31,54 +33,54 @@ class Lyric
      * @SymfonyValidator\NotBlank()
      */
     private $title;
-    
+
     /**
      * @ORM\Column()
      * @SymfonyValidator\NotBlank()
      */
     private $text;
-    
+
     /**
      * @ORM\Column(name="text_bg")
      */
     private $textBg;
-    
+
     /**
      * @ORM\Column(name="extra_info", nullable=true)
      */
     private $extraInfo;
-    
+
     /**
      * @ORM\OneToMany(targetEntity="Tekstove\TekstoveBundle\Entity\Lyric\Translation", mappedBy="lyric")
      */
     private $translations;
-    
+
     /**
      * @ORM\ManyToMany(targetEntity="Tekstove\TekstoveBundle\Entity\Language")
      */
     private $languages;
-    
+
     /**
      * @ORM\ManyToOne(targetEntity="Tekstove\TekstoveBundle\Entity\User")
      * @ORM\JoinColumn(name="uploaded_by", referencedColumnName="id", nullable=true)
      */
     private $uploadedBy;
-    
+
     /**
      * @ORM\Column(name="video_youtube")
      */
     private $videoYoutube;
-    
+
     /**
      * @ORM\Column(name="video_metacafe")
      */
     private $videoMetaCafe;
-    
+
     /**
      * @ORM\Column(name="video_vbox7");
      */
     private $videoVbox7;
-    
+
     /**
      * @ORM\Column(name="cache_title_short")
      */
@@ -88,19 +90,19 @@ class Lyric
      * @ORM\Column()
      */
     private $views;
-    
+
     /**
      * @ORM\Column()
      */
     private $popularity;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->views = 0;
         $this->popularity = 0;
         $this->languages = new ArrayCollection();
     }
 
-    
     /**
      * Get id
      *
@@ -133,88 +135,114 @@ class Lyric
     {
         return $this->title;
     }
-    
-    public function getText() {
+
+    public function getText()
+    {
         return $this->text;
     }
-    
-    public function setText($text) {
+
+    public function setText($text)
+    {
         $this->text = $text;
     }
 
-    public function getTextBg() {
+    public function getTextBg()
+    {
         return $this->textBg;
     }
-    
-    public function getExtraInfo() {
+
+    public function setTextBg($text)
+    {
+        $this->textBg = $text;
+    }
+
+    public function getExtraInfo()
+    {
         return $this->extraInfo;
     }
 
-    public function setExtraInfo($extraInfo) {
+    public function setExtraInfo($extraInfo)
+    {
         $this->extraInfo = $extraInfo;
     }
-            
-    public function getCacheTitleShort() {
+
+    public function getCacheTitleShort()
+    {
         return $this->cacheTitleShort;
     }
 
-    public function setCacheTitleShort($cacheTitleShort) {
+    public function setCacheTitleShort($cacheTitleShort)
+    {
         $this->cacheTitleShort = $cacheTitleShort;
     }
-    
-    public function getViews() {
+
+    public function getViews()
+    {
         return $this->views;
     }
-    
-    public function getPopularity() {
+
+    public function getPopularity()
+    {
         return $this->popularity;
     }
-    
-    public function getTranslations() {
+
+    public function getTranslations()
+    {
         return $this->translations;
     }
-    
-    public function getUploadedBy() {
+
+    public function getUploadedBy()
+    {
         return $this->uploadedBy;
     }
-    
-    public function setUploadedBy($uploadedBy) {
+
+    public function setUploadedBy($uploadedBy)
+    {
         $this->uploadedBy = $uploadedBy;
     }
-    
-    public function getVideoYoutube() {
+
+    public function getVideoYoutube()
+    {
         return $this->videoYoutube;
     }
 
-    public function setVideoYoutube($videoYoutube) {
+    public function setVideoYoutube($videoYoutube)
+    {
         $this->videoYoutube = $videoYoutube;
     }
-    
-    public function getVideoMetaCafe() {
+
+    public function getVideoMetaCafe()
+    {
         return $this->videoMetaCafe;
     }
 
-    public function setVideoMetaCafe($videoMetaCafe) {
+    public function setVideoMetaCafe($videoMetaCafe)
+    {
         $this->videoMetaCafe = $videoMetaCafe;
     }
-    
-    public function getVideoVbox7() {
+
+    public function getVideoVbox7()
+    {
         return $this->videoVbox7;
     }
 
-    public function setVideoVbox7($videoVbox7) {
+    public function setVideoVbox7($videoVbox7)
+    {
         $this->videoVbox7 = $videoVbox7;
     }
-    
-    public function getLanguages() {
+
+    public function getLanguages()
+    {
         return $this->languages;
     }
-    
-    public function addLanguage($language) {
+
+    public function addLanguage(Language $language)
+    {
         $this->languages[] = $language;
     }
-    
-    public function removeLanguage($language) {
+
+    public function removeLanguage(Language $language)
+    {
         $this->languages->removeElement($language);
     }
 }
