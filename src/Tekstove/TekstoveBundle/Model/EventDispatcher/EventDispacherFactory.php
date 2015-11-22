3506dadff7b3005208f34ffff597420750fefe9c
po_taka <angel.koilov@gmail.com>
2015-11-22 13:32:24 +0200

    added subscriber for lyric uploaded by

--- src/Tekstove/TekstoveBundle/Model/EventDispatcher/EventDispacherFactory.php
+++ src/Tekstove/TekstoveBundle/Model/EventDispatcher/EventDispacherFactory.php
@@ -2,18 +2,30 @@
 
 namespace Tekstove\TekstoveBundle\Model\EventDispatcher;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Description of EventDispacherFactory
  *
  * @author po_taka <angel.koilov@gmail.com>
  */
 class EventDispacherFactory {
-    public static function createDispacher()
+    public static function createDispacher(ContainerInterface $container)
     {
         $dispacher = new EventDispacher();
         // add events!
-        $testEvent = new \Tekstove\TekstoveBundle\EventListener\Model\Lyric\LyricTitleCacheSubscriber();
-        $dispacher->addSubscriber($testEvent);
+        $titleCacheSubscriber = new \Tekstove\TekstoveBundle\EventListener\Model\Lyric\LyricTitleCacheSubscriber();
+        
+        $securityTokenStorage = $container->get('security.token_storage');
+        $authzChecker = $container->get('security.authorization_checker');
+        
+        $uploadedBySubscriber = new \Tekstove\TekstoveBundle\EventListener\Model\Lyric\LyricUploadedBySubscriber(
+            $securityTokenStorage,
+            $authzChecker
+        );
+       
+        $dispacher->addSubscriber($titleCacheSubscriber);
+        $dispacher->addSubscriber($uploadedBySubscriber);
         return $dispacher;
     }
 }
