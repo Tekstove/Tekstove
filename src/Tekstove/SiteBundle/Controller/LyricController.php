04e603ca87405f1c0e7704e8341c655af5bcda61
angelk <angel.koilov@gmail.com>
2016-01-02 00:19:54 +0200

    insight fixes

--- src/Tekstove/SiteBundle/Controller/LyricController.php
+++ src/Tekstove/SiteBundle/Controller/LyricController.php
@@ -5,8 +5,6 @@ namespace Tekstove\SiteBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
