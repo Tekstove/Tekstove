859b87f94d3d38c31d2aeee07532a9ff0eafc927
po_taka <angel.koilov@gmail.com>
2016-06-02 21:51:17 +0300

    add knp pagination to kernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -26,6 +26,7 @@ class AppKernel extends Kernel
              * Local
              */
             new Tekstove\SiteBundle\SiteBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
