0f44b999c4e23a8ecdf436dc5860309edfe7633f
po_taka <angel.koilov@gmail.com>
2016-07-09 14:53:35 +0300

    temporary chat on freenode

---
+++ src/Tekstove/SiteBundle/Controller/ChatController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tekstove\SiteBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class ChatController extends Controller
+{
+    /**
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return [];
+    }
+}
