0956a1d328564be5681e205be6fbd8e038695676
po_taka <angel.koilov@gmail.com>
2016-07-15 23:41:55 +0300

    user registration [WIP]

--- src/Tekstove/SiteBundle/Controller/UserController.php
+++ src/Tekstove/SiteBundle/Controller/UserController.php
@@ -2,10 +2,14 @@
 
 namespace Tekstove\SiteBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+
+use Tekstove\SiteBundle\Model\Gateway\Tekstove\Client\Exception\TekstoveValidationException;
 
 /**
  * Description of UserController
@@ -41,18 +45,39 @@ class UserController extends Controller
         ];
     }
     
-    public function registerAction()
+    public function registerAction(Request $request)
     {
         $formBuilder = $this->createFormBuilder();
         $formBuilder->add('username');
-        $formBuilder->add('password');
+        $formBuilder->add('password', PasswordType::class);
         $formBuilder->add('email');
         $formBuilder->add('submit', SubmitType::class);
-        
+        $formBuilder->setMapped('POST');
         $form = $formBuilder->getForm();
         
         $gateway = $this->get('tekstove.gateway.user.register');
         /* @var $gateway \Tekstove\SiteBundle\Model\Gateway\Tekstove\User\RegisterGateway */
+        
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = new \Tekstove\SiteBundle\Model\User\User(
+                [
+                    'username' => $form->get('username')->getData(),
+                    'password' => $form->get('password')->getData(),
+                    'email' => $form->get('email')->getData(),
+                ]
+            );
+            try {
+                $result = $gateway->save($request, $user);
+            } catch (TekstoveValidationException $e) {
+                // @TODO use matcher!
+                foreach ($e->getValidationErrors() as $error) {
+                    $formError = new \Symfony\Component\Form\FormError($error['message']);
+                    $form->addError($formError);
+                }
+            }
+        }
+        
         $gateway->setGroups(['Register']);
         $data = $gateway->find();
         $recaptchaKey = $data['item']['recaptcha']['key'];
--- src/Tekstove/SiteBundle/Model/Gateway/Tekstove/User/RegisterGateway.php
+++ src/Tekstove/SiteBundle/Model/Gateway/Tekstove/User/RegisterGateway.php
@@ -3,8 +3,12 @@
 
 namespace Tekstove\SiteBundle\Model\Gateway\Tekstove\User;
 
-use Tekstove\SiteBundle\Model\Gateway\Tekstove\AbstractGateway;
+use Tekstove\SiteBundle\Model\Gateway\Tekstove\Client\Exception\RequestException;
 
+use Tekstove\SiteBundle\Model\Gateway\Tekstove\AbstractGateway;
+use Tekstove\SiteBundle\Model\Gateway\Tekstove\Client\Exception\TekstoveValidationException;
+
+use Symfony\Component\HttpFoundation\Request;
 use Tekstove\SiteBundle\Model\User\User;
 
 /**
@@ -17,5 +21,40 @@ class RegisterGateway extends AbstractGateway
     protected function getRelativeUrl()
     {
         return '/users/register';
+    }
+    
+    /**
+     * @param Request $request
+     * @param User $user
+     */
+    public function save(Request $request, User $user)
+    {
+        $client = $this->getClient();
+        try {
+            $response = $client->post(
+                $this->getRelativeUrl(),
+                [
+                    'body' => json_encode(
+                        [
+                            'request' => [
+                                'post' => $request->request,
+                            ]
+                        ]
+                    )
+                ]
+            );
+
+            $responseData = $this->decodeBody($response->getBody());
+            return $responseData;
+        } catch (RequestException $e) {
+            if ($e->getCode() != 400) {
+                throw $e;
+            }
+            
+            $validationException = new TekstoveValidationException($e->getMessage(), 0, $e);
+            $errors = json_decode($e->getBody(), true);
+            $validationException->setValidationErrors($errors);
+            throw $validationException;
+        }
     }
 }
