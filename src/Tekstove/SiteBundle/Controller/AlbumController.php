dfe00fb1b40ee0da0c3cae20c9bf8f5cb419766d
po_taka <angel.koilov@gmail.com>
2016-08-27 19:07:07 +0300

    album view

---
+++ src/Tekstove/SiteBundle/Controller/AlbumController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tekstove\SiteBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Tekstove\SiteBundle\Model\Gateway\Tekstove\AbstractGateway;
+
+/**
+ * Description of AlbumController
+ *
+ * @author po_taka <angel.koilov@gmail.com>
+ * @Template()
+ */
+class AlbumController extends Controller
+{
+    public function viewAction($id)
+    {
+        $albumGateway = $this->get('tekstove.gateway.album');
+        /* @var $albumGateway \Tekstove\SiteBundle\Model\Gateway\Tekstove\Album\AlbumGateway */
+        $albumGateway->setGroups([AbstractGateway::GROUP_LIST]);
+        $albumData = $albumGateway->get($id);
+        
+        return [
+            'album' => $albumData['item'],
+        ];
+    }
+}
