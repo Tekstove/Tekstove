a68169d86e1eca71985ac582d294889f39b4356e
po_taka <angel.koilov@gmail.com>
2017-10-08 19:56:13 +0300

    better ads

---
+++ src/Tekstove/SiteBundle/Ads/Ads.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tekstove\SiteBundle\Ads;
+
+class Ads
+{
+    const NOT_ALLOWED = 0;
+    const ALLOWED = 1;
+    const ALLOWED_ONLY_NOT_SAFE = 3;
+
+    const ADS_NAME = [
+        self::NOT_ALLOWED => 'not allowed',
+        self::ALLOWED => 'allowed',
+        self::ALLOWED_ONLY_NOT_SAFE => 'allowed only not safe',
+    ];
+
+    /**
+     * @param int $id
+     * @return string
+     */
+    public function getAdStatusName($id)
+    {
+        if (isset(self::ADS_NAME[$id])) {
+            return self::ADS_NAME[$id];
+        }
+
+        return 'Unknown ad status: ' . $id;
+    }
+}
