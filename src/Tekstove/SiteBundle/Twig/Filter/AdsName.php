cacfa21d42c27ae215a6645369bb8e6f8dced5d6
Angel Koilov (po_taka) <angel.koilov@gmail.com>
2017-10-09 18:59:16 +0300

    better ads (#152)
    * better ads

---
+++ src/Tekstove/SiteBundle/Twig/Filter/AdsName.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tekstove\SiteBundle\Twig\Filter;
+
+use Tekstove\SiteBundle\Ads\Ads;
+
+class AdsName extends \Twig_Extension
+{
+    private $ads;
+
+    /**
+     * @param Ads $ads
+     */
+    public function __construct(Ads $ads)
+    {
+        $this->ads = $ads;
+    }
+
+
+    public function getFilters()
+    {
+        return [
+            new \Twig_SimpleFilter(
+                'ad_name',
+                [
+                    $this,
+                    'adName',
+                ]
+            ),
+        ];
+    }
+
+    public function adName($id)
+    {
+        return $this->ads->getAdStatusName($id);
+    }
+}
