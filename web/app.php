10bf07b625fa8f12f47b3af739312f9f72d2d18e
po_taka <angel.koilov@gmail.com>
2018-01-27 18:17:12 +0200

    fix proxy deprecations

--- web/app.php
+++ web/app.php
@@ -8,20 +8,30 @@ use Symfony\Component\HttpFoundation\Request;
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../var/bootstrap.php.cache';
 
-// Enable APC for autoloading to improve performance.
-// You should change the ApcClassLoader first argument to a unique prefix
-// in order to prevent cache key conflicts with other applications
-// also using APC.
-/*
-$apcLoader = new Symfony\Component\ClassLoader\ApcClassLoader(sha1(__FILE__), $loader);
-$loader->unregister();
-$apcLoader->register(true);
-*/
-
 // see https://github.com/Tekstove/Tekstove/issues/80
 Request::setTrustedHeaderName(Request::HEADER_FORWARDED, null);
-
-//require_once __DIR__.'/../app/AppCache.php';
+Request::setTrustedProxies(
+    [
+        '127.0.0.1',
+        // cloudlfare
+        '103.21.244.0/22',
+        '103.22.200.0/22',
+        '103.31.4.0/22',
+        '104.16.0.0/12',
+        '108.162.192.0/18',
+        '131.0.72.0/22',
+        '141.101.64.0/18',
+        '162.158.0.0/15',
+        '172.64.0.0/13',
+        '173.245.48.0/20',
+        '188.114.96.0/20',
+        '190.93.240.0/20',
+        '197.234.240.0/22',
+        '198.41.128.0/17',
+        '199.27.128.0/21',
+    ],
+    Request::HEADER_X_FORWARDED_FOR
+);
 
 // I know this is ugly...but...sry...
 function errorToException($errNumber, $errMsg, $errFile, $errLine) {
