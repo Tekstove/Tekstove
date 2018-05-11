c6008ab1740153380fad3806569153246c3d4147
po_taka <angel.koilov@gmail.com>
2018-05-11 23:50:03 +0300

    user-delete

--- src/Tekstove/SiteBundle/Controller/UserController.php
+++ src/Tekstove/SiteBundle/Controller/UserController.php
@@ -273,7 +273,6 @@ class UserController extends Controller
 
         $form->add('submit', SubmitType::class);
 
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
