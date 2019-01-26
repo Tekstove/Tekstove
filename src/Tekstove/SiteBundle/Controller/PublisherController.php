9cbf2c99d032a9b3112073b46f35f2c682a727f6
po_taka <angel.koilov@gmail.com>
2019-01-26 22:49:01 +0200

    publisher info

--- src/Tekstove/SiteBundle/Controller/PublisherController.php
+++ src/Tekstove/SiteBundle/Controller/PublisherController.php
@@ -4,6 +4,8 @@ namespace Tekstove\SiteBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Tekstove\SiteBundle\Model\Gateway\Tekstove\Lyric\LyricGateway;
 use Tekstove\SiteBundle\Model\Gateway\Tekstove\Publisher\PublisherGateway;
 
 class PublisherController extends Controller
@@ -11,7 +13,7 @@ class PublisherController extends Controller
     /**
      * @Template()
      */
-    public function viewAction($id)
+    public function viewAction($id, Request $request)
     {
         $gateway = $this->get('app.gateway.v4.publisher');
         /* @var $gateway PublisherGateway */
@@ -19,8 +21,26 @@ class PublisherController extends Controller
         $data = $gateway->get($id);
         $publisher = $data['item'];
 
+        $lyricGateway = $this->get('tesktove.gateway.v4.lyric');
+        /* @var $lyricGateway LyricGateway */
+        $lyricGateway->setGroups([LyricGateway::GROUP_LIST]);
+        $lyricGateway->addOrder('id', LyricGateway::ORDER_DESC);
+        $lyricGateway->addFilter('publisher', $id);
+
+        $paginator = $this->get('knp_paginator');
+        /* @var $paginator \Knp\Component\Pager\Paginator */
+        $lyrics = $paginator->paginate(
+            $lyricGateway,
+            $request->query->getInt('lyricsPage', 1),
+            30,
+            [
+                'pageParameterName' => 'lyricsPage',
+            ]
+        );
+
         return [
             'publisher' => $publisher,
+            'lyrics' => $lyrics,
         ];
     }
 }
