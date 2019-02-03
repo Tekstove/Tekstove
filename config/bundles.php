9da76a568600cca66795d5efa8cb4d98198698c7
po_taka <angel.koilov@gmail.com>
2019-02-03 18:36:02 +0200

    3.4 migration

--- config/bundles.php
+++ config/bundles.php
@@ -8,6 +8,7 @@ return [
     \Tekstove\SiteBundle\SiteBundle::class => ['all' => true],
     \Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     \SunCat\MobileDetectBundle\MobileDetectBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
