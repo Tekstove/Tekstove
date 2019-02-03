ce12c62bab9fd9cbd67ee09997223bb3a3174105
po_taka <angel.koilov@gmail.com>
2019-02-03 22:52:01 +0200

    4.2 migration

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -9,9 +9,7 @@ require dirname(__DIR__).'/vendor/autoload.php';
 if (is_array($env = @include dirname(__DIR__).'/.env.local.php')) {
     $_SERVER += $env;
     $_ENV += $env;
-} elseif (!class_exists(Dotenv::class)) {
-    throw new RuntimeException('Please run "composer require symfony/dotenv" to load the ".env" files configuring the application.');
-} else {
+} elseif (class_exists(Dotenv::class)) {
     // load all the .env files
     (new Dotenv())->loadEnv(dirname(__DIR__).'/.env');
 }
