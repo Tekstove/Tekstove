d8e10d7b53445e456a3a2e1f0b25ef69f7c8abb3
po_taka <angel.koilov@gmail.com>
2020-12-11 01:10:22 +0200

    initial migration to php 7.4

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+PROJECT_NAME=tekstove
+
+# If the script is run with sudo, UID is 0. This is an issue when running
+# "usermod -u $WEB_UID www-data" in the web container.
+# In this case assign WEB_UID to 1000
+[[ $UID == 0 ]] && export WEB_UID=1000 || export WEB_UID=$UID
+
+cd docker
+
+docker-compose -p $PROJECT_NAME build
+docker-compose -p $PROJECT_NAME -f docker-compose.yml up
